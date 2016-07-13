39b587d753c9876d0203ce501a85314d429797f8
Lucas Schejtman <LSchejtman@bauerxcelmedia.co.nz>
2016-07-13 16:39:01 +1200

    input validation

--- src/modules/todo/components/TodoForm.js
+++ src/modules/todo/components/TodoForm.js
@@ -1,3 +1,5 @@
+import R from 'ramda';
+import * as Ru from 'utils/ramda-utils';
 import React, { PropTypes, Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
@@ -9,7 +11,14 @@ class TodoForm extends Component {
 
 	handleSubmit(e) {
 		e.preventDefault();
-		this.props.onSubmit({ ...this.state });
+
+		const el = this.refs.name;
+		const focusEl = element => element.focus();
+		const submit = () => this.props.onSubmit({ ...this.state });
+
+		const handleClick = R.ifElse(Ru.isInputEmpty, focusEl, submit);
+
+		handleClick(el);
 	}
 
 	handleNameChange(e) {
@@ -20,11 +29,16 @@ class TodoForm extends Component {
 		return (
 			<form onSubmit={this.handleSubmit.bind(this)}>
 				<div>
-					<TextField name="name" hintText="Name" floatingLabelText="Name" onChange={this.handleNameChange.bind(this)} />
+					<TextField
+						name="name"
+						ref="name"
+						hintText="Name"
+						floatingLabelText="Name"
+						onChange={this.handleNameChange.bind(this)} />
 					<RaisedButton type="submit" label="Add" />
 				</div>
 			</form>
-		)
+		);
 	}
 }
 
--- src/utils/ramda-utils.js
+++ src/utils/ramda-utils.js
@@ -9,3 +9,6 @@ export const lensById = R.compose(lensMatching, R.propEq('id'));
 export const hasSameProp = prop => R.eqProps(prop);
 export const hasSameId = hasSameProp('id');
 export const rejectById = (id, arr) => R.reject(hasSameId({ id }), arr);
+export const isEmpty = R.equals('');
+export const getInputValue = element => element.getValue();
+export const isInputEmpty = R.compose(isEmpty, R.trim, getInputValue);
